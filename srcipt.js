85d3c304277b6926c6bf6c99c61179947b5fc6d8
Shubhank2002 <maheshwarishubhank2002@gmail.com>
2026-06-28 09:58:02 +0530

    create left section of page where grid and main image will be rendered dynamically via dom manipulation

--- srcipt.js
+++ srcipt.js
@@ -1,9 +1,9 @@
 const wrapper = document.createElement('div')
-wrapper.style.display='flex'
-wrapper.style.padding='20px'
-wrapper.style.justifyContent='space-between'
+wrapper.style.display = 'flex'
+wrapper.style.padding = '20px'
+wrapper.style.justifyContent = 'space-between'
 
-document.appendChild('wrapper')
+document.body.appendChild(wrapper)
 
 const leftSection = document.createElement('div')
 wrapper.appendChild(leftSection)
@@ -14,4 +14,50 @@ wrapper.appendChild(rightSection)
 const mainImage = document.createElement('div')
 mainImage.style.width = '400px'
 mainImage.style.height = '300px'
+mainImage.style.marginBottom = '30px'
 leftSection.appendChild(mainImage)
+
+const grid = document.createElement('div')
+grid.style.display = 'flex'
+grid.style.flexWrap = 'wrap'
+
+const colorData = {
+    blue: ['skyblue', 'lightblue', 'slateblue', 'royalblue', 'darkblue'],
+    red: ['red', 'darkred', 'hotpink'],
+    green: ['yellowgreen', 'limegreen', 'lime', 'green']
+}
+
+function renderThumbnail(filter = 'all') {
+    grid.innerHTML = ''
+
+    if (filter === 'all') {
+        Object.keys(colorData).forEach((color, i) => {
+            colorData[color].forEach((value) => {
+                const thumb = document.createElement('div')
+                thumb.style.width = '100px'
+                thumb.style.height = '100px'
+                thumb.style.margin = '5px'
+                thumb.style.backgroundColor = value
+                thumb.innerText = `${i + 1}/${colorData[color].length} - ${color.toUpperCase()}`
+                grid.appendChild(thumb)
+            })
+        })
+        const firstColor = Object.keys(colorData)[0]  // 'blue'
+        mainImage.style.backgroundColor = colorData[firstColor][0]  // 'skyblue'
+
+    } else {
+        colorData[filter].forEach((value, i) => {
+            const thumb = document.createElement('div')
+            thumb.style.width = '100px'
+            thumb.style.height = '100px'
+            thumb.style.margin = '5px'
+            thumb.style.backgroundColor = value
+            thumb.innerText = `${i + 1}/${colorData[filter].length} - ${filter.toUpperCase()}`
+            grid.appendChild(thumb)
+        })
+    }
+}
+
+leftSection.appendChild(grid)
+
+renderThumbnail('all')
